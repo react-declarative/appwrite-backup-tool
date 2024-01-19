6604748563cb602de53283a45d4643e637cfea6f
Petr Tripolsky <tripolskypetr@gmail.com>
2024-01-19 12:47:46 +0500

    restore-time-order

--- scripts/restore.mjs
+++ scripts/restore.mjs
@@ -12,11 +12,15 @@ const require = Module.createRequire(import.meta.url);
 const schema = require(path.join(process.cwd(), "./appwrite.json"));
 
 const HAS_COLLECTION_ATTRIBUTE = (collectionId, attributeKey) => {
-  const collection = schema.collections.find((item) => item.$id === collectionId);
+  const collection = schema.collections.find(
+    (item) => item.$id === collectionId
+  );
   if (!collection) {
     return false;
   }
-  const attribute = collection.attributes.find((item) => item.key === attributeKey);
+  const attribute = collection.attributes.find(
+    (item) => item.key === attributeKey
+  );
   return !!attribute;
 };
 
@@ -32,9 +36,9 @@ const FILE_UPLOAD_DELAY = 2_000;
 
 const client = new sdk.Client();
 
-client.setEndpoint(process.env.APPWRITE_ENDPOINT)
-client.setProject(process.env.APPWRITE_PROJECT_ID)
-client.setKey(process.env.APPWRITE_API_KEY)
+client.setEndpoint(process.env.APPWRITE_ENDPOINT);
+client.setProject(process.env.APPWRITE_PROJECT_ID);
+client.setKey(process.env.APPWRITE_API_KEY);
 
 if (process.env.APPWRITE_SELF_SIGNED) {
   client.setSelfSigned();
@@ -46,28 +50,22 @@ const storage = new sdk.Storage(client);
 const writeTransform = (data, collectionId) => {
   const userEntries = Object.entries(data)
     .filter(([key]) => !key.startsWith("$"))
-    .filter(([, value]) => value !== null)
-    .filter(([, value]) => {
-      if (Array.isArray(value)) {
-        return value?.length;
-      }
-      return true;
-    })
     .filter(([key]) => {
-      const hasAttribute = HAS_COLLECTION_ATTRIBUTE(collectionId, key)
+      const hasAttribute = HAS_COLLECTION_ATTRIBUTE(collectionId, key);
       if (!hasAttribute) {
-        console.log(`SKIP ATTRIBUTE key=${key} collection=${collectionId}`)
+        console.log(`SKIP ATTRIBUTE key=${key} collection=${collectionId}`);
       }
       return hasAttribute;
     })
   return Object.fromEntries(userEntries);
 };
 
-const sleep = (timeout = 1_000) => new Promise((res) => {
-  setTimeout(() => {
-    res();
-  }, timeout);
-});
+const sleep = (timeout = 1_000) =>
+  new Promise((res) => {
+    setTimeout(() => {
+      res();
+    }, timeout);
+  });
 
 const tryRead = async (databaseId, collectionId, documentId) => {
   try {
@@ -94,9 +92,14 @@ await fs.mkdir("backup/databases", { recursive: true });
 
 // Restore documents
 {
-  const databaseFiles = await glob(`backup/databases/*/*/*.json`);
+  const databaseFiles = await glob(`backup/databases/*/*/*.json`, {
+    withFileTypes: true,
+    stat: true,
+  });
+  databaseFiles.sort((a, b) => a.mtime - b.mtime);
   console.log(`Found ${databaseFiles.length} collection documents`);
-  for (const file of databaseFiles.reverse()) {
+  for (const fileRef of databaseFiles) {
+    const file = fileRef.fullpathPosix();
     const text = await fs.readFile(file);
     const data = JSON.parse(text);
     const { $id, $databaseId, $collectionId } = data;
@@ -104,10 +107,15 @@ await fs.mkdir("backup/databases", { recursive: true });
     try {
       const prevDocument = await tryRead($databaseId, $collectionId, $id);
       if (prevDocument) {
-       //  await databases.updateDocument($databaseId, $collectionId, $id, writeTransform(data, $collectionId))
-        continue
+        await databases.updateDocument($databaseId, $collectionId, $id, writeTransform(data, $collectionId))
+        continue;
       }
-      await databases.createDocument($databaseId, $collectionId, $id, writeTransform(data, $collectionId));
+      await databases.createDocument(
+        $databaseId,
+        $collectionId,
+        $id,
+        writeTransform(data, $collectionId)
+      );
     } finally {
       await sleep(DOCUMENT_WRITE_DELAY);
     }
@@ -124,14 +132,21 @@ await fs.mkdir("backup/databases", { recursive: true });
     console.log(`Found ${files.length} files in ${bucketId}`);
     for (const file of files) {
       const fileName = path.basename(file);
-      const fileId = fileName.slice(0, fileName.length - path.extname(fileName).length);
+      const fileId = fileName.slice(
+        0,
+        fileName.length - path.extname(fileName).length
+      );
       console.log(`Uploading ${fileId} from ${bucketId}`);
       try {
         if (await isFileExist(bucketId, fileId)) {
           console.log(`File ${fileId} ${bucketId} exist, skip`);
           continue;
         }
-        await storage.createFile(bucketId, fileId, InputFile.fromPath(file, fileName)); 
+        await storage.createFile(
+          bucketId,
+          fileId,
+          InputFile.fromPath(file, fileName)
+        );
       } finally {
         await sleep(FILE_UPLOAD_DELAY);
       }
