e2413502cc64671e2bdce2843240467f94e8937f
Petr Tripolsky <tripolskypetr@gmail.com>
2024-07-18 14:01:03 +0500

    backup-collection

---
+++ scripts/backup-collection.mjs
@@ -0,0 +1,139 @@
+import sdk from "node-appwrite";
+
+import Module from "module";
+import dotenv from "dotenv";
+import { existsSync } from 'fs';
+import fs from "fs/promises";
+import { glob } from "glob";
+import path from "path";
+
+const require = Module.createRequire(import.meta.url);
+
+if (!existsSync(".env")) {
+  console.log("Missing .env file. Please use .env.example as a template");
+  process.exit(-1);
+}
+
+const schema = require(path.join(process.cwd(), "./appwrite.json"));
+const entries = process.argv.slice(2);
+
+if (!entries.length) {
+  throw new Error('collection list not provided');
+}
+
+const FIND_COLLECTION = (name) => {
+  const collection = schema.collections.find(
+    (item) => item.name === name
+  );
+  if (!collection) {
+    throw new Error(`collection not found name=${name}`);
+  }
+  return collection;
+};
+
+dotenv.config();
+
+const DOCUMENT_READ_DELAY = 100;
+
+const TOTAL_DOCUMENTS_LIMIT = 100_000;
+const DOCUMENTS_PAGE_SIZE = 100;
+
+const client = new sdk.Client();
+
+client.setEndpoint(process.env.APPWRITE_ENDPOINT)
+client.setProject(process.env.APPWRITE_PROJECT_ID)
+client.setKey(process.env.APPWRITE_API_KEY)
+
+if (process.env.APPWRITE_SELF_SIGNED) {
+  client.setSelfSigned();
+}
+
+const databases = new sdk.Databases(client);
+
+const sleep = (timeout = 1_000) => new Promise((res) => {
+  setTimeout(() => {
+    res();
+  }, timeout);
+});
+
+const listDocuments = async function* (databaseId, collectionId, queries = []) {
+  queries = queries.filter((value) => !!value);
+  let lastId = null;
+
+  const createRequest = async () => {
+    console.log(`Fetching collection ${collectionId} from ${collectionId} starting from ${lastId || 0}`)
+    return await databases.listDocuments(
+      databaseId,
+      collectionId,
+      [
+        sdk.Query.orderDesc("$updatedAt"),
+        ...(lastId ? [sdk.Query.cursorAfter(lastId)] : []),
+        ...queries,
+        sdk.Query.limit(DOCUMENTS_PAGE_SIZE),
+      ]
+    );
+  };
+
+  let counter = 0;
+  let lastQuery = createRequest();
+
+  while (counter < TOTAL_DOCUMENTS_LIMIT) {
+    const [{ documents }] = await Promise.all([
+      lastQuery,
+      sleep(DOCUMENT_READ_DELAY),
+    ]);
+    for (const document of documents) {
+      yield document;
+    }
+    if (documents.length < DOCUMENTS_PAGE_SIZE) {
+      break;
+    }
+    lastId = documents[documents.length - 1].$id;
+    lastQuery = createRequest();
+    counter += documents.length;
+  }
+};
+
+await fs.mkdir("backup/databases", { recursive: true });
+
+// Backup documents
+{
+  const { databases: databasesList, total } = await databases.list();
+  console.log(`Found ${total} databases!`);
+  for (const database of databasesList) {
+    const collections = entries.map(FIND_COLLECTION);
+    console.log(`Found ${total} collections id ${database.$id}!`);
+    for (const collection of collections) {
+      console.log(`Making backup of ${collection.$id}`);
+      await fs.mkdir(`backup/databases/${database.$id}/${collection.$id}`, {
+        recursive: true,
+      });
+      for await (const document of listDocuments(database.$id, collection.$id)) {
+        console.log(
+          `Writing ${document.$id} from ${collection.$id} from ${database.$id}`
+        );
+        await fs.writeFile(
+          `backup/databases/${database.$id}/${collection.$id}/${document.$id}.json`,
+          JSON.stringify(document, null, 2)
+        );
+      }
+    }
+  }
+}
+
+{
+  console.log("Checking backup consistence...");
+  const databaseFiles = await glob(`backup/databases/*/*/*.json`);
+  let isOk = true;
+  for (const file of databaseFiles) {
+    try {
+      JSON.parse(await fs.readFile(file));
+    } catch {
+      isOk = false;
+      console.log(`${file} is broken!`);
+    }
+  }
+  if (isOk) {
+    console.log("Everything is OK");
+  }
+}
